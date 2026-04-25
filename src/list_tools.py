25bb6b50fc1922c8622c2b9bb1507360059bd3a6
VirtualCalhoun <johncalhounjohn@gmail.com>
2026-04-24 22:41:09 -0400

    Initialization program list-tools

---
+++ src/list_tools.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def parse_args():
+    pass
+
+
+def compute_stats(numbers):
+    pass
+
+
+def main():
+    numbers = parse_args()
+    compute_stats(numbers)
+
+
+if __name__ == "__main__":
+    main()
